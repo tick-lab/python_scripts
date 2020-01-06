7f7c9f48eb8856391e865496db1229f0e1899b9a
aemann01 <aemann01@ou.edu>
2020-01-06 15:32:09 -0600

    added quick script to add missing taxonomy levels to list of taxa

---
+++ fix_taxonomy_L7.py
@@ -0,0 +1,16 @@
+import sys
+import pandas as pd
+
+newtax = []
+
+with open(sys.argv[1], "r") as f:
+	for line in f:
+		x = line
+		y = x.count(";")
+		if y < 6:
+			add = 6 - y
+			new = ";unknown" * add
+			print(x + new)
+		else:
+			print(x)
+			
